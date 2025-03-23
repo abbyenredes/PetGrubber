14daf5dc3691309b06e64e7843dfd4c8d1dcae0e
AbbyEnRedes <abbyenredes@gmail.com>
2025-03-23 18:44:39 +0100

    feat (scrapper funcional)

---
+++ main.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import random
+import time
+import pandas as pd
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import undetected_chromedriver as uc
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# configuracion de navegador
+def main():
+    browser = uc.Chrome()
+    url = 'https://www.tiendanimal.es/flamingo-sticks-de-madera-para-roer-/FLA7370_M.html'
+    browser.get(url)
+    browser.implicitly_wait(10)
+    # automatizacion para aceptar cookies
+    cookie_button = WebDriverWait(browser, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))
+    cookie_button.click()
+    html = browser.page_source
+    soup = bs(html, 'lxml')
+    soup
+    # browser.close() # cerrar pestaña
+    input("Presiona Enter para cerrar el navegador...")
+    browser.quit() # cerrar navegador
+    
+    # extraccion de datos
+    product_name = soup.find('h1', class_='product-page-title-block__title').text
+    print(product_name)
+
+    product_price = soup.find('span', class_='product-page-action__price').text
+    print(product_price)
+    
+    product_description = soup.find('div', class_='product-page-details__content is-long').text
+    print(product_description)
+    
+    product_rating = soup.find('div', class_='product-page-title-block__rating').text
+    print(product_rating)
+    
+    ''''
+    product_stars = soup.find('div', class_='product-page-title-block__stars').text
+    print(product_stars)
+    '''
+
+
+if __name__ == '__main__':
+    main()
